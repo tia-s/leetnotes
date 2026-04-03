9d46a14e0d758ef5f0ba7089ea6134be8ba27d57
tia-s <tiakristensalmon@gmail.com>
2026-04-03 20:23:08 -0300

    Update httplink for dev and add ruby version

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: process.env.VITE_API_URL + '/graphql',
+  uri: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL + '/graphql' : '/graphql',
 });
 
 export const client = new ApolloClient({
